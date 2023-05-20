eb48cfaa19b0f7355121aa2cd03ae66382293f14
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-05-20 16:37:27 -0400

    big change - change canvas populate from flex to css grid, add change size, color mode, and erase feature.

--- app.js
+++ app.js
@@ -1,36 +1,88 @@
-let controls = []
+const canvas = document.getElementById('canvas');
+const sizeInput = document.getElementById('myNumber')
+const changeSizeBtn = document.getElementById('changeSizeBtn')
+const eraseBtn = document.getElementById('erase')
 
-let canvas = document.getElementById('canvas');
+const colorBtn = document.getElementById('color')
+const classicBtn = document.getElementById('classic')
+let colorMode = 'classic';
 
-let squareBySquare = 25;
 
-//iterate, squarebysquare times 
-  //iterate again squarebysquare times
-    //const square = create a div
-    //add a class called square -->
-    //canvas.appendChild(square)
-    
 
-for(let i = 0; i < squareBySquare; i++) {
-  const row = document.createElement('div')
-  for(let j = 0; j < squareBySquare; j++) {
-     const square = document.createElement('div')
-     square.classList.add('square');
-     row.appendChild(square)
+function populateSquare(size = 16) {
+  canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+  canvas.style.gridTemplateRows = `repeat(${size}, 1fr)`
+
+  for(let i = 0; i < size*size; i++) {
+    let grid = document.createElement('div');
+    grid.classList.add('square');
+    canvas.appendChild(grid)
   }
-  canvas.appendChild(row);
+}
+function getRandomColor() {
+  const r = Math.floor(Math.random() * 256); // Random red value between 0 and 255
+  const g = Math.floor(Math.random() * 256); // Random green value between 0 and 255
+  const b = Math.floor(Math.random() * 256); // Random blue value between 0 and 255
+
+  const color = `rgb(${r}, ${g}, ${b})`; // Construct the RGB color string
+
+  return color;
 }
 
-function hoverChange(e) {
-  this.style.backgroundColor = 'rgb(112, 112, 112)'
+
+//add event listener on on the button
+//when clicked hover change pass in the value
+function hoverChange() {
+  if (colorMode === 'classic') this.style.backgroundColor = 'rgb(112, 112, 112)';
+  if (colorMode === 'color') this.style.backgroundColor = getRandomColor();
+}
+function eraseCanvas() {
+  const squares = document.querySelectorAll('.square')
+  squares.forEach(square => {
+    square.style.backgroundColor = '#D8D8D8'
+  })
 }
 
-const squares = document.querySelectorAll('.square');
+function changeSize(size) {
+  if(size === '') {size = 16}
+  
+  //reset the grid
+  canvas.innerHTML = ''
 
-squares.forEach( square => {
-  square.addEventListener('mouseover', hoverChange)
-})
+  //then populate
+  canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+  canvas.style.gridTemplateRows = `repeat(${size}, 1fr)`
 
-console.log(squares)
+  for(let i = 0; i < size*size; i++) {
+    let grid = document.createElement('div');
+    grid.classList.add('square');
+    canvas.appendChild(grid)
+  }
+
+  const squares = document.querySelectorAll('.square');
+  squares.forEach(square => {
+    square.addEventListener('mouseover', hoverChange)
+  })  
+}
 
 
+function turnOnEtch() {
+  populateSquare()
+  const squares = document.querySelectorAll('.square');
+  squares.forEach(square => {
+    square.addEventListener('mouseover', hoverChange)
+  })  
+}
+
+turnOnEtch()
+
+
+changeSizeBtn.addEventListener('click', () => changeSize(sizeInput.value))
+colorBtn.addEventListener('click', () => colorMode = 'color')
+classicBtn.addEventListener('click', () => color = 'classic')
+eraseBtn.addEventListener('click', eraseCanvas)
+
+
+//rainbow mode - changes color 
+
+// alpha mode
